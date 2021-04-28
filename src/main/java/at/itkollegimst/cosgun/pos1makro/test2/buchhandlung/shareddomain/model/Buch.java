e0f5f98f0144e360f277e85b3978acc0f0dbf2b7
turanc84 <tcosgun@tsn.at>
2021-04-28 16:27:05 +0200

    shareddomain Buchbestellung Buch Eigenschaften, Constructor, Getter Setter

---
+++ src/main/java/at/itkollegimst/cosgun/pos1makro/test2/buchhandlung/shareddomain/model/Buch.java
@@ -0,0 +1,48 @@
+package at.itkollegimst.cosgun.pos1makro.test2.buchhandlung.shareddomain.model;
+
+public class Buch
+{
+    String name;
+    String buchnummer;
+    float preis;
+    boolean buchdruck;
+
+    public Buch(String name, String buchnummer, float preis, boolean buchdruck) {
+        this.name = name;
+        this.buchnummer = buchnummer;
+        this.preis = preis;
+        this.buchdruck = buchdruck;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBuchnummer() {
+        return buchnummer;
+    }
+
+    public void setBuchnummer(String buchnummer) {
+        this.buchnummer = buchnummer;
+    }
+
+    public float getPreis() {
+        return preis;
+    }
+
+    public void setPreis(float preis) {
+        this.preis = preis;
+    }
+
+    public boolean isBuchdruck() {
+        return buchdruck;
+    }
+
+    public void setBuchdruck(boolean buchdruck) {
+        this.buchdruck = buchdruck;
+    }
+}
