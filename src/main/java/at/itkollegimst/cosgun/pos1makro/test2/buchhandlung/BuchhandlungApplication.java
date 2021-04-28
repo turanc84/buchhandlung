6447eeb478b7bfcb20b7f04be4538fab95dcb0bd
turanc84 <tcosgun@tsn.at>
2021-04-28 14:18:47 +0200

    Start vom Projekt

---
+++ src/main/java/at/itkollegimst/cosgun/pos1makro/test2/buchhandlung/BuchhandlungApplication.java
@@ -0,0 +1,13 @@
+package at.itkollegimst.cosgun.pos1makro.test2.buchhandlung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuchhandlungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuchhandlungApplication.class, args);
+    }
+
+}
