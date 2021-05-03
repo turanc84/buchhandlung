9892a7df4e58b7c651f5988493db9488a7139318
turanc84 <tcosgun@tsn.at>
2021-05-03 20:53:27 +0200

    alle packeges...die files sind vorübergehend sonst kann ich keine packages anlegen

--- buchhandlung/src/main/java/at/itkollegimst/cosgun/pos1makro/test2/buchhandlung/domain/Laaaaa.java
+++
@@ -1,4 +0,0 @@
-package at.itkollegimst.cosgun.pos1makro.test2.buchhandlung.domain;
-
-public class Laaaaa {
-}
--- buchhandlung/src/main/java/at/itkollegimst/cosgun/pos1makro/test2/buchhandlung/infrastructure/testi.java
+++
@@ -1,4 +0,0 @@
-package at.itkollegimst.cosgun.pos1makro.test2.buchhandlung.infrastructure;
-
-public interface testi {
-}
