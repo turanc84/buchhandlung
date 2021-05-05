719c32353f26c1228db702c6ee216e153c89199b
turanc84 <tcosgun@tsn.at>
2021-05-06 01:20:20 +0200

    bestellungservice/services/ KLasse BuchbestellungQueryService nicht ganz fertig kleiner fehler zum suchen zu spät schaue ich morgen

---
+++ bestellungservice/src/main/java/com/example/bestellungservice/services/BuchbestellungQueryService.java
@@ -0,0 +1,34 @@
+package com.example.bestellungservice.services;
+
+
+import com.example.bestellungservice.domain.aggregate.Buchbestellung;
+import com.example.bestellungservice.exception.BuchbestellungNotFoundException;
+import com.example.bestellungservice.infrastructure.repositories.BuchbestellungRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BuchbestellungQueryService {
+
+    private final BuchbestellungRepository buchbestellungRepository;
+
+    public BuchbestellungQueryService(BuchbestellungRepository buchbestellungRepository){
+
+        this.buchbestellungRepository = buchbestellungRepository;
+
+
+    }
+
+
+    public Buchbestellung getBuchbestellungByBestellnummer(String bestellnummer) throws BuchbestellungNotFoundException{
+
+        return buchbestellungRepository.getBuchbestellungByBestellnummer(bestellnummer).orElse(BuchbestellungNotFoundException::new);
+
+    }
+
+    public boolean bestellnummerExisting(String bestellnummer){
+
+        return buchbestellungRepository.getBuchbestellungByBestellnummer(bestellnummer).isPresent();
+    }
+
+
+}
